a65ba7621eb4b285e7e8165efcd899b7264ba167
Rajesh <54771023+iamrajesh-CODER@users.noreply.github.com>
2020-10-16 19:56:29 +0530

    Update App.js

--- Chat App/client/src/App.js
+++ Chat App/client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Chat from './components/Chat/Chat';
+
 import Join from './components/Join/Join';
 
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -12,6 +13,6 @@ const App = () => {
       <Route path="/chat" component={Chat} />
     </Router>
   );
+  
 }
-
 export default App;
